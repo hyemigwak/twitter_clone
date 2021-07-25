ab199c1f413b7c7415834cb72dd81fff5610737e
hyemigwak <goadl92@gmail.com>
2021-07-25 20:22:36 +0900

    리덕스 설치 및 리듀서 생성

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { Menu, Input, Row, Col } from "antd";
 import UserProfile from "../components/UserProfile";
 import LoginForm from "../components/LoginForm";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
 
 const AppLayout = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -39,7 +41,7 @@ const AppLayout = ({ children }) => {
       <Row gutter={8}>
         {/* sm={}은 태블릿용 */}
         <Col xs={24} md={6}>
-          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
         </Col>
         <Col xs={24} md={12}>
           {children}
---
+++ front/reducers/index.js
@@ -0,0 +1,62 @@
+const initialState = {
+  user: {
+    isLoggedIn: false,
+    user: null,
+    signUpData: {},
+    loginData: {},
+  },
+  post: {
+    mainPosts: [],
+  },
+};
+
+export const loginAction = (data) => {
+  return {
+    type: "LOG_IN",
+    data,
+  };
+};
+
+export const logoutAction = () => {
+  return {
+    type: "LOG_OUT",
+  };
+};
+
+//async action creator
+
+//action creator, 중복되는 액션들을 매번 만들 수 없으니 함수로 만들었다.
+const changeNickname = (data) => {
+  return {
+    type: "CHANGE_NICKNAME",
+    data,
+  };
+};
+changeNickname("amy");
+store.dispatch(changeNickname("amy2"));
+
+// (이전상태, 액션) => 다음 상태
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOG_IN":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isLoggedIn: true,
+          user: action.data,
+        },
+      };
+    case "LOG_OUT":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          isLoggedIn: false,
+          user: null,
+        },
+      };
+  }
+};
+
+export default rootReducer;
